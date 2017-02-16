3cfae80c39367bda285edf557e8e2db66d7a81b0
SepKamil <sepkamil@gmail.com>
2017-02-16 16:38:44 +0100

    Corrected naming mistake and updated xpi

--- chrome/content/messenger.js
+++ chrome/content/messenger.js
@@ -1,4 +1,20 @@
 var st = {
+    
+    ui: {
+        
+        promptservice: Components.classes['@mozilla.org/embedcomp/prompt-service;1'].getService(Components.interfaces.nsIPromptService),
+
+        elementMap: function (id) {
+
+            if (this.map === undefined)
+                this.map = {};
+
+            if (this.map[id] === undefined)
+                this.map[id] = document.getElementById(id);
+
+            return this.map[id];
+        },
+    },
     steganosaurusRead: function () {
 
         let hiddentext;
@@ -10,11 +26,12 @@ var st = {
             else hiddentext="";
             });
 
-        if (hiddentext.localeCompare("") == 0){
-            alert('Wiadomość nie zawiera ukrytej treści');
+        if (hiddentext.localeCompare("") != 0){
+            st.ui.promptservice.alert(hiddentext);
         }
         else {
-            alert(hiddentext);
+            
+            st.ui.promptservice.alert('Wiadomość nie zawiera ukrytej treści');
         }
     }
 
